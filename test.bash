7b56151ef9d2af8f32bfaf2a2a395cc5b451e4ba
Jesse Zhang <jzhang@pivotal.io>
2016-10-31 23:06:51 -0700

    Run shellcheck on ERB-rendered bash scripts
    Also, switch to container-based builds on Travis to get faster feedback
    [closes #2]

--- test.bash
+++ test.bash
@@ -1,18 +1,51 @@
 #!/bin/bash
 
 set -e -u -o pipefail
+set -o posix
 
 _main() {
-	local -a SHELLCHECK_OPTS
-	SHELLCHECK_OPTS=("$@")
-
-	# Per BASH documentation, ARR=() is considered unset
-	shell_scripts | xargs shellcheck --shell bash "${SHELLCHECK_OPTS[@]+${SHELLCHECK_OPTS[@]}}"
+	shell_scripts | xargs shellcheck --shell bash
+	render_and_lint $(erb_shell_scripts)
 }
 
 shell_scripts() {
-	find jobs packages -name '*.bash' -perm -001
+	find test -name '*.bash'
 	find packages -name 'packaging' -or -name 'pre_packaging'
+}
+
+erb_shell_scripts() {
+	find jobs -name '*.bash' -or -name '*.bash.erb'
+}
+
+render_and_lint() {
+	local -a erbs=("$@")
+
+	local -a bashes
+	bashes=($(render_in_tmp "${erbs[@]}"))
+
+	local errno
+	(
+	cd tmp
+	if shellcheck --shell bash "${bashes[@]}"; then
+		true
+	else
+		errno=$?
+		return "${errno}"
+	fi
+	)
+}
+
+render_in_tmp() {
+	local erb
+	local bash
+	local tmp_bash
+	for erb in "$@"; do
+		bash=${erb%.erb}
+		tmp_bash="tmp/${bash}"
+		mkdir -p "$(dirname "${tmp_bash}")"
+		test/render.rb "${erb}" > "${tmp_bash}"
+		echo "${bash}"
+	done
 }
 
 _main "$@"
